ae139a9f0cf67e66e79e8997c471171f89c90374
贫僧信赖海飞丝 <1406070130@qq.com>
2019-01-04 17:30:05 +0800

    第一个例子

---
+++ chapter1/src/main/java/com/springboot/charpter1/Chapter1Main.java
@@ -0,0 +1,26 @@
+package com.springboot.charpter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@EnableAutoConfiguration
+public class Chapter1Main {
+    @RequestMapping("/test")
+    @ResponseBody
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key", "value");
+        return map;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter1Main.class, args);
+    }
+}
